c485dd7e6b9edd1aa952be412299aa113d00d032
Aarón Juárez <66045880+Aaronga19@users.noreply.github.com>
2021-06-29 11:30:33 -0500

    Update Exercises4.py
    some excercices done

--- Exercices/E4/Exercises4.py
+++ Exercices/E4/Exercises4.py
@@ -5,12 +5,34 @@ def cube(x):
     return x
 print(f"The cube of 2 is:", cube(2))"""
 
-"""---"""
-
+#------------------------------------------------------------------------------------------------------------
+"""
 def mistery(x):
     y = 0 
     for value in x:
         y+=value**2
-    return
+    return y
+print(f"La suma de los cuadrados de esta lista es: {mistery(x=[1,2,3,4,5])}")"""
 
-print(mistery(x=[1,2,3,4,5]))
+#------------------------------------------------------------------------------------------------------------
+"""
+def seconds_since_midnigth(hrs,minut,sec):
+    hours_in_seconds = (hrs)*(60*60)
+    minutes_in_seconds = (minut*60)
+    return hours_in_seconds+minutes_in_seconds+sec
+print(f"Han pasado: {int(seconds_since_midnigth(1,60,2))} segundos.")
+"""
+#------------------------------------------------------------------------------------------------------------
+"""
+import datetime as dt
+print(dt.datetime.today())
+"""
+#------------------------------------------------------------------------------------------------------------
+"""print(round(8.536841))"""
+
+#------------------------------------------------------------------------------------------------------------
+grados = float(input("Inserte temperatura en 'Celcius':"))
+def celciusToFarenheit(Celcius):
+    Far = (9/5)*Celcius+32
+    return round(Far,3)
+print(f"{grados} grados Celcius son: {celciusToFarenheit(grados)} grados Farenheit")
